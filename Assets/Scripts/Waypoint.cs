2cea774c1b618f09615784d8cbe7e6831bb9e51d
A01352283 <a01352283@itesm.mx>
2023-05-16 22:48:13 -0600

    Adds patrol for the Bulborb

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] protected float debugDrawRadius = 1.0f;
+
+    public virtual void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+}
